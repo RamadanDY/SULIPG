bc05cbda0f384fb39cc3768a4583b34836054328
Ramadan <daudalaramadan2@gmail.com>
2024-05-17 11:24:33 +0000

    hmm

--- Server/index.js
+++ Server/index.js
@@ -3,9 +3,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const cors = require("cors");
+const morgan = require("morgan");
 const EmployeeModel = require("./Models/Employee.jsx");
 
 const app = express();
+
+app.use(morgan("tiny"));
 // transfers the data into json format
 app.use(express.json());
 app.use(cors());
@@ -15,13 +18,16 @@ app.use(cors());
 // the model is like how we want our db to look like in our db
 // ie like specifying the data types
 app.post("/register", (req, res) => {
+  console.log("REGISTERING...");
   EmployeeModel.create(req.body)
     .then((employees) => res.json(employees))
     .catch((err) => req.json(err));
 });
 
 // creates a connecttion with the db
-mongoose.connect("mongodb://localhost:27017/employee");
+mongoose.connect("mongodb://localhost:27017/employee").then(() => {
+  console.info("Successfully connected to the Database.");
+});
 const Port = 3004;
 app.listen(Port, () => {
   console.log("server is running on: ", Port);
--- registerpg/src/Signup-Login/LoginSignup.jsx
+++ registerpg/src/Signup-Login/LoginSignup.jsx
@@ -4,7 +4,7 @@ import email_icon from '../Assets/email.png';
 import password_icon from '../Assets/password.png';
 import person_icon from '../Assets/person.png';
 import { Link } from "react-router-dom";
-
+import axios from 'axios';
 
 export const LoginSignup = () => {
     // Define state variables
@@ -15,38 +15,39 @@ export const LoginSignup = () => {
     // Define handleSubmit function
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log('Client Registering');
         axios.post('http://localhost:3004/register', { name, email, password })
             .then(result => console.log(result))
             .catch(error => console.log(error));
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-        <div className='container md' >
-            <div className="header">
-                <div className="text">Sign-Up</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                <div className="input" >
-                    <img src={person_icon} alt="" />
-                    <input type="text" placeholder='Name' autoFocus onChange={(e) => setName(e.target.value)} />
+        <div className='container md'>
+            <form onSubmit={handleSubmit}>
+                <div className="header">
+                    <div className="text">Sign-Up</div>
+                    <div className="underline"></div>
                 </div>
-                <div className="input" >
-                    <img src={email_icon} alt="" />
-                    <input type="email" placeholder='Email id' onChange={(e) => setEmail(e.target.value)} />
+                <div className="inputs">
+                    <div className="input">
+                        <img src={person_icon} alt="" />
+                        <input type="text" placeholder='Name' autoFocus onChange={(e) => setName(e.target.value)} />
+                    </div>
+                    <div className="input">
+                        <img src={email_icon} alt="" />
+                        <input type="email" placeholder='Email id' onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="input">
+                        <img src={password_icon} alt="" />
+                        <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+                    </div>
                 </div>
-                <div className="input">
-                    <img src={password_icon} alt="" />
-                    <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+                <div className="forgot-password">lost Password? <span>Click here</span></div>
+                <div className="submit-container">
+                    <button className="submit" type='submit'>Sign Up</button>
+                    <Link to="/signup" className="submit" type='submit'>Login</Link>
                 </div>
-            </div>
-            <div className="forgot-password">lost Password? <span>Click here</span></div>
-            <div className="submit-container">
-                <button className="submit" type='submit'>Sign  Up</button>
-                <Link to="/signup" className="submit" type='submit'>Login</Link>
-            </div>
+            </form>
         </div>
-        </form>
     );
 };
